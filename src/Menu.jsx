13d15d8bcbf4bc884982172c63983d2dcee60579
Noirspider <83585087+Noirspider@users.noreply.github.com>
2024-02-12 11:40:40 +0100

    Snellito menu, css bottoni, aggiunta classe per font

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -7,26 +7,20 @@ import shuttle from "./shuttle.png";
 function Menu() {
   return (
     <Container className="d-flex h-100 my-auto flex-column align-items-center justify-content-center fs-1 display-1 fst-italic  ">
-      <Link to={"/game"}>
-        <Row className="selection">
-          <Col className="text-white">Play</Col>
-        </Row>
+      <Link to={"/game"} className=" mb-3 font-Silkscreen" id="button-menu">
+        Play
       </Link>
       <div className="flex Div-logo position-relative position-absolute">
         <div className="div-shuttle">
           <img src={shuttle} className="App-logos" alt="logo" />
         </div>
-        <img src={fire} className="App-logos rotate" alt="logo" />
+        <img src={fire} className="App-logos rotate " alt="logo" />
       </div>
-      <Link to={"/"}>
-        <Row className="selection">
-          <Col className="text-white">Options</Col>
-        </Row>
+      <Link to={"/"} className=" font-Silkscreen  mb-3" id="button-menu">
+        Options
       </Link>
-      <Link to={"/"}>
-        <Row className="selection">
-          <Col className="text-white">Settings</Col>
-        </Row>
+      <Link to={"/"} className=" mb-3 font-Silkscreen" id="button-menu">
+        Settings
       </Link>
     </Container>
   );
